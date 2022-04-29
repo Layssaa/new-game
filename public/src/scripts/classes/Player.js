8706f4c8a60b1cb7b2d5b63a96e138e19be88806
Ermerson Camarão Lima <el.erm.lima@gmail.com>
2022-04-29 19:53:02 -0300

    Create: index logic (#3)
    * create: index content
    
    * improve: add button
    
    * create: enter nickname logic
    
    * create: player and ws class
    
    * create: exit function
    
    * fix: buttons type
    
    * refactor: lang case for SEO
    
    * fix: function declaration
    
    * create: refactor function call
    
    * create: error store on class
    
    * create: add and remove player from playersWS
    
    * create: gitignore new addition
    
    * create: gitignore new addition
    
    * fix: change port connection
    
    * create: add export default
    
    * fix: change script location
    
    * create: add import

---
+++ public/src/scripts/classes/Player.js
@@ -0,0 +1,35 @@
+class Player {
+  constructor(_nickname) {
+    this.nickname = _nickname;
+    this.socket = new WebSocket("ws://localhost:5050");
+    this.disabled = false;
+    this.errors = [];
+  }
+
+  controls(enable) {
+    if (enable) {
+      disabled = true;
+    } else {
+      disabled = false;
+    }
+  }
+
+  init() {
+    this.socket.onopen = () => {
+      this.controls(false);
+    };
+
+    this.socket.onerror = (err) => {
+      this.errors.push({
+        created_at: new Date(),
+        error: err,
+      });
+    };
+  }
+
+  exit() {
+    this.socket.close();
+  }
+}
+
+export default Player;
---
+++ public/src/scripts/enter.js
@@ -0,0 +1,57 @@
+import { enterForm, mockGame } from "./html-content";
+import Player from "./classes/Player";
+import feedbacks from "./messages/feedbacks";
+import { playersWS } from "../../../backend/__mock__/data-mock";
+
+const onLoadIndexHtml = () => {
+  const rootDiv = document.querySelector("#root");
+  let newPlayer = undefined;
+
+  try {
+    rootDiv.innerHTML = enterForm;
+
+    const nickname = document.querySelector("#nickname");
+    const enterButton = document.querySelector("#enter-button");
+    const feedbackMessage = document.querySelector("#feedback-message");
+
+    const setMessage = (text) => {
+      feedbackMessage.innerHTML = text;
+      setTimeout(() => {
+        feedbackMessage.innerHTML = "";
+      }, 2000);
+    };
+
+    const enter = () => {
+      if (/\s/g.test(nickname.value)) {
+        setMessage(feedbacks.BLANK_SPACE);
+      } else if (nickname.value.length <= 0) {
+        setMessage(feedbacks.SHORT_NICKNAME);
+      } else {
+        playersWS.push({
+          nickname: nickname,
+          created_at: new Date(),
+        });
+        newPlayer = new Player(nickname.value);
+        rootDiv.innerHTML = mockGame;
+        document.querySelector("#exit-button").addEventListener("click", exit);
+      }
+    };
+
+    const exit = () => {
+      const newPlayersWS = playersWS.filter((playerWS) => {
+        return playerWS.nickname !== newPlayer.nickname;
+      });
+      playersWS = newPlayersWS;
+      newPlayer.exit();
+      rootDiv.innerHTML = enterForm;
+    };
+
+    enterButton.addEventListener("click", enter);
+  } catch (error) {
+    rootDiv.innerHTML = "Página em manuntenção";
+  }
+};
+
+onLoadIndexHtml();
+
+export default onLoadIndexHtml;
