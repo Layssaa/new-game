0ef1f79a3a1a43374cdf2405fdf1d976a28dc6cf
BgFerreira <brunogabriel.ff@gmail.com>
2022-04-30 17:36:42 -0300

    Improving Renderizer

--- public/src/scripts/runMaze.js
+++ public/src/scripts/runMaze.js
@@ -2,10 +2,11 @@ import { MatrixGenerator } from "./classes/MatrixGenerator.js";
 import { MazeRenderizer } from "./classes/MazeRenderizer.js";
 import { PathCreator } from "./classes/PathCreator.js";
 
-const mazeMatrix = new MatrixGenerator(13, 13, 2);
+const mazeMatrix = new MatrixGenerator(20, 30, 2);
 const matrix = mazeMatrix.getMatrix();
 
+const mazePath = new PathCreator(matrix);
+const path = mazePath.getPath();
 
-
-const maze = new MazeRenderizer(matrix);
+const maze = new MazeRenderizer(matrix, path);
 maze.renderizeMaze();
