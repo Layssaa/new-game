58d902af3ee3f479e95c0d34c4d94c537476778e
Bruno Ferreira <brunogabriel.ff@gmail.com>
2022-05-01 20:57:31 -0300

    Merge move maze (#9)
    * Adicionando a base do código de movimentos do player para ser adaptado no código do labirinto
    
    * Adicionando meu labirinto com comentários explicativos
    
    * Created maze generator
    
    * Divided responsibily of MazeGenerator
    
    * Improving Renderizer
    
    * juntando código do movimento com labirinto
    
    * fix: error undefined props in class
    
    * Trabalhando com movimento
    
    * Movimento funcionando
    
    * fix: movimento, e labirinto
    
    Co-authored-by: Isabella Firmino <isabella.mfbr@gmail.com>
    Co-authored-by: Layssa <layssa.lima166@gmail.com>

---
+++ public/src/scripts/classes/MazeRenderizer.js
@@ -0,0 +1,243 @@
+export class Game {
+  constructor(_canvas, _context) {
+    this.canvas = _canvas;
+    this.context = _context;
+    this.canvasWidth = this.canvas.width;
+    this.canvasHeight = this.canvas.height;
+    this.tileSize = 72;
+    this.loop = function () {};
+  }
+
+  renderizeMaze() {
+    this.createMaze();
+    this.createCamera();
+    this.createPlayer();
+  }
+
+  renderizeCanvas() {
+    this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+    this.context.save();
+
+    this.context.translate(-this.camera.x, -this.camera.y);
+
+    for (let row in this.matrix) {
+      for (let column in this.matrix[row]) {
+        let tile = this.matrix[row][column];
+        if (tile === 1) {
+          let x = column * this.tileSize;
+          let y = row * this.tileSize;
+          this.context.fillRect(x, y, this.tileSize, this.tileSize);
+        }
+      }
+    }
+
+    this.context.fillStyle = "#00f";
+    this.context.fillRect(
+      this.player.x,
+      this.player.y,
+      this.player.width,
+      this.player.height
+    );
+    this.context.restore();
+  }
+
+  createMaze() {
+    this.matrix = [
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 3, 1, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1],
+      [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+      [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+      [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+      [1, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+      [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+      [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+      [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+      [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+      [1, 3, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ];
+
+    this.walls = [];
+    this.exits = [];
+    this.starts = [];
+
+    this.mazeWidth = this.matrix[0].length * this.tileSize;
+    this.mazeHeight = this.matrix.length * this.tileSize;
+
+    for (let row in this.matrix) {
+      for (let column in this.matrix[row]) {
+        let tile = this.matrix[row][column];
+
+        if (tile === 1) {
+          let wall = {
+            x: this.tileSize * column,
+            y: this.tileSize * row,
+            width: this.tileSize,
+            height: this.tileSize
+          };
+
+          this.walls.push(wall);
+        }
+
+        if (tile === 2) {
+          let exit = {
+            x: this.tileSize * column,
+            y: this.tileSize * row,
+            width: this.tileSize,
+            height: this.tileSize
+          };
+
+          this.exits.push(exit);
+        }
+
+        if (tile === 3) {
+          let start = {
+            x: this.tileSize * column,
+            y: this.tileSize * row,
+            width: this.tileSize,
+            height: this.tileSize
+          };
+
+          this.starts.push(start);
+        }
+      }
+    }
+
+    this.selectStart();
+  }
+
+  createCamera() {
+    this.camera = {
+      x: 0,
+      y: 0,
+      width: this.canvasWidth,
+      height: this.canvasHeight,
+      innerLeftBoundary: function () {
+        return this.x + this.width * 0.25;
+      },
+      innerTopBoundary: function () {
+        return this.y + this.height * 0.25;
+      },
+      innerRightBoundary: function () {
+        return this.x + this.width * 0.75;
+      },
+      innerBottomBoundary: function () {
+        return this.y + this.height * 0.75;
+      },
+    };
+  }
+
+  createPlayer() {
+    this.player = {
+      x: this.starts[this.startRandom].x + (this.tileSize / 2 - this.tileSize / 4),
+      y: this.starts[this.startRandom].y + (this.tileSize / 2 - this.tileSize / 4),
+      width: this.tileSize / 2,
+      height: this.tileSize / 2,
+      speed: 8,
+    };
+  }
+
+  update(_left, _up, _right, _down, _downListener, _upListener) {
+    if (_left && !_right) {
+      this.player.x -= this.player.speed;
+    } else if (_right && !_left) {
+      this.player.x += this.player.speed;
+    } else if (_up && !_down) {
+      this.player.y -= this.player.speed;
+    } else if (_down && !_up) {
+      this.player.y += this.player.speed;
+    }
+
+    for (let i in this.walls) {
+      let wall = this.walls[i];
+      this.wallCollision(this.player, wall);
+    }
+
+    for(let i in this.exits){
+      let exit = this.exits[i];
+      this.endCollision(this.player, exit);
+    }
+
+    if (this.player.x < this.camera.innerLeftBoundary()) {
+      this.camera.x = this.player.x - this.camera.width * 0.25;
+    }
+    if (this.player.y < this.camera.innerTopBoundary()) {
+      this.camera.y = this.player.y - this.camera.height * 0.25;
+    }
+    if (this.player.x + this.player.width > this.camera.innerRightBoundary()) {
+      this.camera.x =
+        this.player.x + this.player.width - this.camera.width * 0.75;
+    }
+    if (
+      this.player.y + this.player.height >
+      this.camera.innerBottomBoundary()
+    ) {
+      this.camera.y =
+        this.player.y + this.player.height - this.camera.height * 0.75;
+    }
+    this.camera.x = Math.max(
+      0,
+      Math.min(this.mazeWidth - this.camera.width + 150, this.camera.x)
+    );
+    this.camera.y = Math.max(
+      0,
+      Math.min(this.mazeHeight - this.camera.height + 150, this.camera.y)
+    );
+  }
+
+  wallCollision(objA, objB) {
+    let distX = objA.x + objA.width / 2 - (objB.x + objB.width / 2);
+    let distY = objA.y + objA.height / 2 - (objB.y + objB.height / 2);
+
+    let sumWidth = (objA.width + objB.width) / 2;
+    let sumHeight = (objA.height + objB.height) / 2;
+
+    if (Math.abs(distX) < sumWidth && Math.abs(distY) < sumHeight) {
+      let overlapX = sumWidth - Math.abs(distX);
+      let overlapY = sumHeight - Math.abs(distY);
+
+      if (overlapX > overlapY) {
+        objA.y = distY > 0 ? objA.y + overlapY : objA.y - overlapY;
+      } else {
+        objA.x = distX > 0 ? objA.x + overlapX : objA.x - overlapX;
+      }
+    }
+  }
+
+  endCollision(objA, objB) {
+    let distX = objA.x + objA.width / 2 - (objB.x + objB.width / 2);
+    let distY = objA.y + objA.height / 2 - (objB.y + objB.height / 2);
+
+    let sumWidth = (objA.width + objB.width) / 2;
+    let sumHeight = (objA.height + objB.height) / 2;
+
+    if (Math.abs(distX) < sumWidth && Math.abs(distY) < sumHeight) {
+      let overlapX = sumWidth - Math.abs(distX);
+      let overlapY = sumHeight - Math.abs(distY);
+
+      if (overlapX > overlapY || !(overlapX > overlapY)) {
+        // Bloqueiar o listener, pop up de vitória, requisição
+      }
+    }
+  }
+
+  selectStart() {
+    this.startRandom = Math.floor(Math.random() * this.starts.length);
+  }
+}
---
+++ public/src/scripts/runMazeIsa.js
@@ -0,0 +1,76 @@
+import { Game } from "./classes/MazeRenderizer.js";
+
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+const keys = {
+  left: 37,
+  up: 38,
+  right: 39,
+  down: 40
+};
+
+const move = {
+  left: false,
+  up: false,
+  right: false,
+  down: false,
+};
+
+window.addEventListener("keydown", keyDownHandler, false);
+window.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(_e) {
+  let key = _e.keyCode;
+
+  switch (key) {
+    case keys.left:
+      move.left = true;
+      break;
+
+    case keys.up:
+      move.up = true;
+      break;
+
+    case keys.right:
+      move.right = true;
+      break;
+
+    case keys.down:
+      move.down = true;
+      break;
+  }
+}
+
+function keyUpHandler(_e) {
+  let key = _e.keyCode;
+
+  switch (key) {
+    case keys.left:
+      move.left = false;
+      break;
+
+    case keys.up:
+      move.up = false;
+      break;
+
+    case keys.right:
+      move.right = false;
+      break;
+
+    case keys.down:
+      move.down = false;
+      break;
+  }
+}
+
+const game = new Game(canvas, context);
+
+game.loop = () => {
+  game.update(move.left, move.up, move.right, move.down);
+  game.renderizeCanvas();
+  requestAnimationFrame(game.loop, canvas);
+}
+
+requestAnimationFrame(game.loop, canvas);
+game.renderizeMaze();
