e355056eaf5ccfdfa9506da453d191c8098f839d
Ermerson Camarão Lima <el.erm.lima@gmail.com>
2022-05-01 21:05:34 -0300

    fix: fix type button text

--- public/src/scripts/html-content/enter-form.js
+++ public/src/scripts/html-content/enter-form.js
@@ -2,7 +2,7 @@ const enterForm = `
 <form id="enter-form">
 <label for="nickname">Nome:</label>
 <input type="text" id="nickname" maxlength="32" /><br />
-<button typ="button" id="enter-button">Entrar</button><br />
+<button type="button" id="enter-button">Entrar</button><br />
 <div id="feedback-message"></div>
 </form>
 `;
