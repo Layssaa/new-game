3c493ec08d884518d5e9704550d90cae90673c63
Ermerson Camarão Lima <el.erm.lima@gmail.com>
2022-05-02 19:18:54 -0300

    improve: export and use of enter.js variables (#18)
    * create: run prettier
    
    * create: add exports on scripts
    
    * create: new const rootDiv

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-import onLoadIndexHtml from "./src/scripts/enter";
+import { onLoadIndexHtml } from "./src/scripts/enter";
 import "./src/scripts/index";
 import "./src/styles/style.css";
 
--- public/src/scripts/enter.js
+++ public/src/scripts/enter.js
@@ -4,51 +4,47 @@ import feedbacks from "./messages/feedbacks";
 import { playersWS } from "../../../backend/__mock__/data-mock";
 import { makeGame } from "./runMazeIsa";
 
-const onLoadIndexHtml = () => {
-  let newPlayer = undefined;
+export let newPlayer = undefined;
+export const rootDiv = document.querySelector("#root");
 
-  try {
-    const setMessage = (text) => {
-      const feedbackMessage = document.querySelector("#feedback-message");
-      feedbackMessage.innerHTML = text;
-      setTimeout(() => {
-        feedbackMessage.innerHTML = "";
-      }, 2000);
-    };
+export const onLoadIndexHtml = () => {
+  rootDiv.innerHTML = enterForm;
+  document.querySelector("#enter-button").addEventListener("click", enter);
+};
 
-    const enter = () => {
-      const nickname = document.querySelector("#nickname").value;
-      if (/\s/g.test(nickname)) {
-        setMessage(feedbacks.BLANK_SPACE);
-      } else if (document.querySelector("#nickname").value.length <= 0) {
-        setMessage(feedbacks.SHORT_NICKNAME);
-      } else {
-        playersWS.push({
-          nickname: document.querySelector("#nickname").value,
-          created_at: new Date(),
-        });
-        newPlayer = new Player(document.querySelector("#nickname").value);
-        document.querySelector("#root").innerHTML = maze;
-        makeGame();
-        document.querySelector("#exit-button").addEventListener("click", exit);
-      }
-    };
+export const setMessage = (text) => {
+  const feedbackMessage = document.querySelector("#feedback-message");
+  feedbackMessage.innerHTML = text;
+  setTimeout(() => {
+    feedbackMessage.innerHTML = "";
+  }, 2000);
+};
 
-    const exit = () => {
-      const newPlayersWS = playersWS.filter((playerWS) => {
-        return playerWS.nickname !== newPlayer.nickname;
-      });
-      playersWS = newPlayersWS;
-      newPlayer.exit();
-      newPlayer = undefined;
-      document.querySelector("#root").innerHTML = enterForm;
-      document.querySelector("#enter-button").addEventListener("click", enter);
-    };
-    document.querySelector("#root").innerHTML = enterForm;
-    document.querySelector("#enter-button").addEventListener("click", enter);
-  } catch (error) {
-    document.querySelector("#root").innerHTML = "Página em manuntenção";
+export const enter = () => {
+  const nickname = document.querySelector("#nickname").value;
+  if (/\s/g.test(nickname)) {
+    setMessage(feedbacks.BLANK_SPACE);
+  } else if (document.querySelector("#nickname").value.length <= 0) {
+    setMessage(feedbacks.SHORT_NICKNAME);
+  } else {
+    playersWS.push({
+      nickname: document.querySelector("#nickname").value,
+      created_at: new Date(),
+    });
+    newPlayer = new Player(document.querySelector("#nickname").value);
+    rootDiv.innerHTML = maze;
+    makeGame();
+    document.querySelector("#exit-button").addEventListener("click", exit);
   }
 };
 
-export default onLoadIndexHtml;
+export const exit = () => {
+  const newPlayersWS = playersWS.filter((playerWS) => {
+    return playerWS.nickname !== newPlayer.nickname;
+  });
+  playersWS = newPlayersWS;
+  newPlayer.exit();
+  newPlayer = undefined;
+  rootDiv.innerHTML = enterForm;
+  document.querySelector("#enter-button").addEventListener("click", enter);
+};
