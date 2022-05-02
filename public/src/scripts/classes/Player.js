3044e28024b06e25598646c626e78d270c1394dd
Ermerson Camarão Lima <el.erm.lima@gmail.com>
2022-05-01 21:24:29 -0300

    fix: websocket url

--- public/src/scripts/classes/Player.js
+++ public/src/scripts/classes/Player.js
@@ -1,7 +1,7 @@
 class Player {
   constructor(_nickname) {
     this.nickname = _nickname;
-    this.socket = new WebSocket("ws://localhost:5050");
+    this.socket = new WebSocket("wss://localhost:5050");
     this.disabled = false;
     this.errors = [];
     this.channel = "general";
