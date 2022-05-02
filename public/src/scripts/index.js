8fdb47c268cda78e72892183bedbe9578fd42857
Ermerson Camarão Lima <el.erm.lima@gmail.com>
2022-05-02 18:32:36 -0300

    create: run prettier

--- public/src/scripts/index.js
+++ public/src/scripts/index.js
@@ -1,6 +1,5 @@
-export * from "./enter"
-export * from "./runMazeIsa"
-export * from "./classes/index"
-export * from "./html-content/index"
-export * from "./messages/index"
-
+export * from "./enter";
+export * from "./runMazeIsa";
+export * from "./classes/index";
+export * from "./html-content/index";
+export * from "./messages/index";
