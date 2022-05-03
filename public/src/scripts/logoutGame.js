86c9c0bbce9edecb10caaf30d3fde771ef4b4aa9
Fernando Neto <65270585+FernandoNeto96@users.noreply.github.com>
2022-05-03 01:09:42 -0300

    bug fix logout rootDiv export

--- public/src/scripts/logoutGame.js
+++ public/src/scripts/logoutGame.js
@@ -1,4 +1,4 @@
-import { newPlayer } from "./enter.js";
+import { newPlayer, rootDiv } from "./enter.js";
 import enterForm from "./html-content/enter-form.js";
 import logout from "./html-content/logout";
 
