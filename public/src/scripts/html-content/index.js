b9ed9e9ff58ea958f08b258497216db98117e1a0
Layssa <layssa.lima166@gmail.com>
2022-05-03 11:32:46 -0300

    chore: code refactoring

--- public/src/scripts/html-content/index.js
+++ public/src/scripts/html-content/index.js
@@ -1,5 +1,5 @@
 import enterForm from "./enter-form";
-import mockGame from "./mock-game";
-import {maze} from "./maze";
+import mockGame from "./maze-matrix";
+import { maze } from "./maze";
 
-export { enterForm, mockGame,maze };
+export { enterForm, mockGame, maze };
