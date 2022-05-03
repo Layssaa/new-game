bbc89fe849eef59a92c4649215e43fa6bb8435ac
Layssa <layssa.lima166@gmail.com>
2022-05-03 16:33:38 -0300

    feat: add onmessage event request to player

--- public/src/scripts/runMazeIsa.js
+++ public/src/scripts/runMazeIsa.js
@@ -2,8 +2,10 @@ import { Game } from "./classes/MazeRenderizer.js";
 import { mazeMatrix } from "./html-content/index.js";
 import { exit } from "./logoutGame.js";
 import { keyDownHandler, keyUpHandler, move } from "./keys-control";
+import { endGame, receivedData, sendWalk } from "./request-control.js";
 
 let game = undefined;
+const id = localStorage.getItem("id");
 
 export const makeGame = () => {
   const canvas = document.querySelector("canvas");
@@ -19,8 +21,34 @@ export const makeGame = () => {
   });
 
   requestAnimationFrame(game.loop, canvas);
+
+  game.setRequestTimeOut(function (move) {
+    return sendWalk({ move, id });
+  });
+
   game.renderizeMaze();
+  game.setEndGame(endGame);
 };
+
+function readPaths(response) {
+  const res = JSON.parse(response.data);
+  console.log("run ws read paths");
+  console.log(res);
+
+  if (res.path === "login") {
+    console.log("set id from login");
+    const receivedId = res.id;
+    localStorage.setItem("id", receivedId);
+    game.setInfoPlayer({ id: receivedId, name: mockName });
+  }
+
+  if (res.path === "walk" && res.id !== id) {
+    console.log("set moves");
+    game.setMovesPlayers(res);
+  }
+}
 
 window.addEventListener("keydown", keyDownHandler, false);
 window.addEventListener("keyup", keyUpHandler, false);
+
+receivedData(readPaths);
