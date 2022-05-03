dbbfb59a51f0f45d4723a7796ea7ab3a086124a4
Ermerson Camarão Lima <el.erm.lima@gmail.com>
2022-05-03 15:55:19 -0300

    refactor: translate variables names to english (#28)
    * refactor: translate fundo word to english
    
    * refactor: translate frogs words to english
    
    * fix: run prettier
    
    * fix: run prettier
    
    * refactor: update names

--- public/src/scripts/html-content/index.js
+++ public/src/scripts/html-content/index.js
@@ -1,5 +1,5 @@
 import enterForm from "./enter-form";
-import mockGame from "./maze-matrix";
+import mazeMatrix from "./maze-matrix";
 import { maze } from "./maze";
 
-export { enterForm, mockGame, maze };
+export { enterForm, mazeMatrix, maze };
--- public/src/scripts/logoutGame.js
+++ public/src/scripts/logoutGame.js
@@ -1,9 +1,8 @@
 import { newPlayer, rootDiv } from "./enter.js";
 import enterForm from "./html-content/enter-form.js";
 
-
 export const exit = () => {
-    newPlayer.exit();
-    newPlayer = undefined
-    rootDiv.innerHTML = enterForm;
-  };
+  newPlayer.exit();
+  newPlayer = undefined;
+  rootDiv.innerHTML = enterForm;
+};
