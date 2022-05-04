a65d29bf2406a3b98d0e5ea7f58862f6261952ff
Ermerson Camarão Lima <el.erm.lima@gmail.com>
2022-05-04 00:03:57 -0300

    create: baril export

--- public/src/scripts/index.js
+++ public/src/scripts/index.js
@@ -1,7 +1,7 @@
-export * from "./enter"
-export * from "./runMazeIsa"
-export * from "./classes/index"
-export * from "./html-content/index"
-export * from "./messages/index"
-export * from "./logoutGame"
-
+export * from "./enter";
+export * from "./runMazeIsa";
+export * from "./classes/index";
+export * from "./html-content/index";
+export * from "./messages/index";
+export * from "./logoutGame";
+export * from "./winnerPopUp";
