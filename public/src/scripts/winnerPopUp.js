42a3ceb2cb9a726c0f18c20c3ae30f733f8e40aa
Ermerson Camarão Lima <el.erm.lima@gmail.com>
2022-05-03 23:59:12 -0300

    refactor: export module

--- public/src/scripts/winnerPopUp.js
+++ public/src/scripts/winnerPopUp.js
@@ -1,9 +1,7 @@
-const winnerPopUp = (winner) => {
+export const winnerPopUp = (winner) => {
   const popUp = document.querySelector("#winner-pop-up");
   popUp.style.backgroundColor = "rgba(53, 52, 49, 0.95)";
   let p = document.createElement("p");
   popUp.append(p);
   p.innerHTML = `${winner} ganhou!`;
 };
-
-export default winnerPopUp;
