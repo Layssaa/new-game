1cf23d7ac003df22713b46f0274dd2ca3f519822
Ermerson Camarão Lima <el.erm.lima@gmail.com>
2022-05-04 00:41:18 -0300

    fix: exit code

--- public/src/scripts/exit.js
+++ public/src/scripts/exit.js
@@ -1,9 +1,9 @@
 import { rootDiv } from "./enter.js";
 import enterForm from "./html-content/enter-form.js";
-import { ws } from "./request-control.js";
+import { ws, logoutWS } from "./request-control.js";
 
 export const exit = () => {
-  ws.exit();
+  logoutWS();
   ws = undefined;
   rootDiv.innerHTML = enterForm;
 };
