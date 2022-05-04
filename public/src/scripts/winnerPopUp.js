a804d3f5fae1c7ff78eea61495749cde23a09b35
Ermerson Camarão Lima <el.erm.lima@gmail.com>
2022-05-04 01:19:42 -0300

    create: change show winner value

--- public/src/scripts/winnerPopUp.js
+++ public/src/scripts/winnerPopUp.js
@@ -1,7 +1,7 @@
-export const winnerPopUp = (winner) => {
+export const winnerPopUp = () => {
   const popUp = document.querySelector("#winner-pop-up");
   popUp.style.backgroundColor = "rgba(53, 52, 49, 0.95)";
   let p = document.createElement("p");
   popUp.append(p);
-  p.innerHTML = `${winner} ganhou!`;
+  p.innerHTML = `${localStorage.getItem("nickname")} ganhou!`;
 };
