e065c9cf3a01ca10c91a383a01834f74e03ea115
Ermerson Camarão Lima <el.erm.lima@gmail.com>
2022-05-03 23:50:49 -0300

    refactor: add export defaault

--- public/src/scripts/winnerPopUp.js
+++ public/src/scripts/winnerPopUp.js
@@ -1,8 +1,9 @@
-    
-export const winnerPopUp = (winner) => {
-    const popup = document.getElementById('popupVencedor')
-    popup.style.backgroundColor = 'rgba(53, 52, 49, 0.95)';
-    let p = document.createElement("p");
-    popup.append(p);
-    p.innerHTML = `${winner} ganhou!`
-}
+const winnerPopUp = (winner) => {
+  const popup = document.getElementById("popupVencedor");
+  popup.style.backgroundColor = "rgba(53, 52, 49, 0.95)";
+  let p = document.createElement("p");
+  popup.append(p);
+  p.innerHTML = `${winner} ganhou!`;
+};
+
+export default winnerPopUp;
