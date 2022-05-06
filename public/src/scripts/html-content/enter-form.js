b00b10c25ec380e63dedbc2b146bc63950786771
BgFerreira <brunogabriel.ff@gmail.com>
2022-05-05 23:01:04 -0300

    bug do enter resolvido

--- public/src/scripts/html-content/enter-form.js
+++ public/src/scripts/html-content/enter-form.js
@@ -4,13 +4,13 @@ const enterForm = `
         <h1>Alfrog</h1>
         <div class='sapin'></div>
     </div>
-    <form id="enter-form">
+    <div id="enter-form">
         <div>
             <input placeholder="Insira seu nome" type="text" id="nickname" maxlength="32" />
         </div>
         <button type="button" id="enter-button">Entrar</button><br />
         <div id="feedback-message"></div>
-    </form>
+    </div>
 </div>
 `;
 
